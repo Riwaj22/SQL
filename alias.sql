489ef11d52e151ac50d9c77ba56e6436d99473b1
Riwaj Neupane <99485058+Riwaj22@users.noreply.github.com>
2023-04-23 12:42:58 +0545

    alias used to provide meaningful name

---
+++ alias.sql
@@ -0,0 +1,8 @@
+-- alias
+
+-- alias is used to assign an alias to the column or a table
+
+-- select column name as column alias
+-- from table name
+
+select Customer_id as "Serial Number", Customer_name as name , age as customer_age from customer
